2831f2c7aa161bee1b96ea6053ff77c6be913685
Thanh Nguyen <thanh.nguyen04@sap.com>
2018-05-26 18:36:09 +0700

    fix crypto test

--- test/core/crypto.spec.js
+++ test/core/crypto.spec.js
@@ -1,7 +1,8 @@
 const MemoryStream = require('memorystream');
-const {createCipher, createDecipher} = require('../../src/core/crypto');
+const { createCipher, createDecipher } = require('../../src/core/crypto');
+const { promisify } = require('util');
 
-describe('encrypt', () => {
+describe('crypto', () => {
     let stream;
     beforeEach(() => {
         stream = new MemoryStream([]);
@@ -23,18 +24,16 @@ describe('encrypt', () => {
 
     it('should create decrypted data', (done) => {
         const encrypt = createCipher('Abcd1234');
-        const decrypt = createDecipher('Abcd1234');
+        const decrypt = createDecipher('Abcd1234').setEncoding('utf8');
         const message = 'The quick brown fox jumps over the lazy dog';
 
         stream.pipe(encrypt).pipe(decrypt);
-        let data;
-        stream.on('data', (chunk) => {
-            data = chunk.toString();
-        });
-        stream.on('end', () => {
-            expect(data).toEqual(message);
+        stream.on('end',()=>{
+            decrypt.end();
+            expect(decrypt.read()).toEqual(message);
             done();
         });
         stream.end(message);
+
     }, 5000);
 });
